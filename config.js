eac1324322b17bc41b6f048968e739920dbeae66
L1h7uu <166722909+L1h7uu@users.noreply.github.com>
2026-07-04 10:07:20 +0700

    Update message and link in config.js

--- config.js
+++ config.js
@@ -13,8 +13,8 @@ const CONFIG = {
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/vohoang1407?locale=vi_VN' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
